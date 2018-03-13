0c73029289d34adbc866e8085d82ddd2e681c714
Tanner Norton <tanneranorton23@gmail.com>
2018-03-12 18:13:49 -0600

    final stuffs

--- lab-4-csmaster23/public/script.js
+++ lab-4-csmaster23/public/script.js
@@ -6,6 +6,7 @@ var app = new Vue({
     show: 'all',
     drag: {},
     priority: '',
+    sorted: false,
   },
   created: function() {
     this.getItems();
@@ -40,6 +41,17 @@ var app = new Vue({
           if(item.priority === 'Low')
             return item;
       });
+      /*if (this.sorted === true)
+        return this.items.filter(function(item) {
+          if(item.priority === 'High')
+            return item;
+          else if (item.priority === 'Medium')
+            return item;
+          else if (item.priority === 'Low')
+            return item;
+      });*/
+      
+
       return this.items;
     },
   },
@@ -93,12 +105,77 @@ var app = new Vue({
       }).catch(err => {
       });
     },
+    increment: function(item) {
+      if (item.priority === "Medium") {
+        console.log(item.priority);
+        item.priority = "High";
+        console.log(item.priority);
+      }
+      if (item.priority === "Low") {
+        item.priority = "Medium";
+      }
+
+      axios.put("/api/items/" + item.id, {
+        text: item.text,
+        priority: item.priority,
+        completed: item.completed,
+  orderChange: false,
+      }).then(response => {
+        return true;
+      }).catch(err => {
+      });
+    },
+    decrement: function(item) {
+      if (item.priority === "Medium") {
+        console.log(item.priority);
+        item.priority = "Low";
+        console.log(item.priority);
+      }
+      if (item.priority === "High") {
+        item.priority = "Medium";
+      }
+
+      axios.put("/api/items/" + item.id, {
+        text: item.text,
+        priority: item.priority,
+        completed: item.completed,
+  orderChange: false,
+      }).then(response => {
+        return true;
+      }).catch(err => {
+      });
+    },
     deleteItem: function(item) {
       axios.delete("/api/items/" + item.id).then(response => {
   this.getItems();
   return true;
       }).catch(err => {
       });
+    },
+    sortedFun: function() {
+      /*console.log(this.sorted);*/
+      this.sorted = true;
+      var high = [];
+      var med = [];
+      var low = [];
+      var a_length = this.items.length;
+      for (i = 0; i < a_length; i++) {
+
+        if (this.items[i].priority === 'High')
+          high.push(this.items[i]);
+        if (this.items[i].priority === 'Medium')
+          med.push(this.items[i]);
+        if (this.items[i].priority === 'Low')
+          low.push(this.items[i]);
+      }//end for
+      var temp = [];
+      var tempi = [];
+      temp = high.concat(med);
+      tempi = temp.concat(low);
+      this.items = tempi;
+      /*console.log(this.items);*/
+      return this.items;
+
     },
     showAll: function() {
       this.show = 'all';
@@ -141,3 +218,49 @@ var app = new Vue({
     },
   }
 });
+
+
+
+
+/*if (this.sorted === true) {
+        var a_length = this.items.length;
+        var temp;
+        var tempi;
+        var running = true;
+        while (running) {
+          for (i = 0; i < a_length; i++) {
+
+            if (this.items[i].priority === 'High')
+              continue;
+
+            if (this.items[i].priority === 'Medium')
+              if ((i+1) > a_length)
+                continue;
+              if (this.items[i+1].priority === 'High')
+                temp = this.items[i];
+                this.items[i] = this.items[i+1];
+                this.items[i+1] = temp;
+
+            if (this.items[i].priority === 'Low')
+              if ((i+1) > a_length)
+                continue;
+              if (this.items[i+1].priority === 'Medium')
+                tempi = this.items[i];
+                this.items[i] = this.items[i+1];
+                this.items[i+1] = tempi;
+          }//end for
+          for (i = 0; i < a_length; i++) {
+            if ((i+1) > a_length)
+                i = i-1;
+            if (this.items[i].priority === 'Medium' && this.items[i+1].priority === 'High')
+              continue;
+            if (this.items[i].priority === 'Low' && this.items[i+1].priority === 'Medium')
+              continue;
+            if (this.items[i].priority === 'Low' && this.items[i+1].priority === 'High')
+              continue;
+            running = false;
+            i = i + 1;
+          }
+        }//end while
+        return this.items
+      }//end if*/
